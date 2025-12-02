f57bc00a9970d7c0b37606c444aae736e2b55468
webmaster442 <ruzsinszki.gabor@gmail.com>
2025-12-02 16:40:04 +0100

    Examples (WIP)

---
+++ LowSharp/Examples/ExampleLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Xml.Serialization;
+
+namespace LowSharp.Examples;
+
+internal sealed class ExampleLoader
+{
+    private readonly ExamplesRoot _root;
+
+    public ExampleLoader()
+    {
+        XmlSerializer serializer = new XmlSerializer(typeof(ExamplesRoot));
+        using (var stream = typeof(ExampleLoader).Assembly.GetManifestResourceStream("LowSharp.Examples.Examples.xml")!)
+        {
+            _root = (ExamplesRoot)serializer.Deserialize(stream)!;
+        }
+    }
+
+    public IEnumerable<Example> GetCsharpExamples()
+    {
+        return _root.Examples.Where(e => e.Type == "Csharp");
+    }
+
+}
