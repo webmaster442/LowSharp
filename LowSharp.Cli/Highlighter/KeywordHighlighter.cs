566bdda9bc2cc6a35e4636e4539d1aed1e60dd57
webmaster442 <ruzsinszki.gabor@gmail.com>
2025-12-07 22:07:44 +0100

    Work on CLI version

---
+++ LowSharp.Cli/Highlighter/KeywordHighlighter.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+
+using Spectre.Console;
+
+namespace LowSharp.Cli.Highlighter;
+
+internal abstract class KeywordHighlighter
+{
+    private readonly HashSet<string> _keywords;
+
+    protected KeywordHighlighter()
+    {
+        _keywords = new HashSet<string>(GetKeywords(), Comparision);
+    }
+
+    protected abstract IEqualityComparer<string> Comparision { get; }
+
+    protected abstract IEnumerable<string> GetKeywords();
+
+    public string HighlightKeyWords(string input, Color keywordColor)
+    {
+        StringBuilder builder = new StringBuilder(input.Length);
+        using var reader = new StringReader(input);
+        string? line;
+
+        while ((line = reader.ReadLine()) != null)
+        {
+            var words = line.Split();
+            foreach (var word in words)
+            {
+                if (_keywords.Contains(word))
+                {
+                    builder.Append($"[bold {keywordColor}]{word.EscapeMarkup()}[/]");
+                }
+                else
+                {
+                    builder.Append(word);
+                }
+                builder.Append(' ');
+            }
+            builder.AppendLine();
+        }
+
+        return builder.ToString();
+    }
+}
---
+++ LowSharp.Cli/Highlighter/VisualBasicHighlighter.cs
@@ -0,0 +1,29 @@
+﻿namespace LowSharp.Cli.Highlighter;
+
+internal sealed class VisualBasicHighlighter : KeywordHighlighter
+{
+    protected override IEqualityComparer<string> Comparision => throw new NotImplementedException();
+
+    protected override IEnumerable<string> GetKeywords()
+    {
+        return
+        [
+            "#Const", "#Else", "#ElseIf", "#End", "#If", "&", "&=", "*", "*=", "+", "+=", "-", "-=",
+            "/", "/=", "<<", "<<=", "=", ">>", ">>=", "AddHandler", "AddressOf", "Alias", "And", "AndAlso",
+            "As", "Boolean", "ByRef", "ByVal", "Byte", "CBool", "CByte", "CChar", "CDate", "CDbl", "CDec", 
+            "CInt", "CLng", "CObj", "CSByte", "CShort", "CSng", "CStr", "CType", "CUInt", "CULng", "CUShort",
+            "Call", "Case", "Catch", "Char", "Class", "Const", "Continue", "Date", "Decimal", "Declare", "Default",
+            "Delegate", "Dim", "DirectCast", "Do", "Double", "Each", "Each", "Else", "ElseIf", "End", "EndIf", "Enum", 
+            "Erase", "Error", "Event", "Exit", "False", "Finally", "For", "Friend", "Function", "Get", "GetType", 
+            "GetXMLNamespace", "Global", "GoSub", "GoTo", "Handles", "If", "Implements", "Imports", "In", "Inherits",
+            "Integer", "Interface", "Is", "Is", "IsNot", "Let", "Lib", "Like", "Long", "Loop", "Me", "Mod", "Module", 
+            "MustInherit", "MustOverride", "MyBase", "MyClass", "NameOf", "Namespace", "Narrowing", "New", "Next",
+            "Not", "NotInheritable", "NotOverridable", "Nothing", "Object", "Of", "On",  "Operator", "Option", "Optional",
+            "Or", "OrElse", "Out", "Overloads", "Overridable", "Overrides", "ParamArray", "Partial", "Private", "Property",
+            "Protected", "Public", "REM", "RaiseEvent", "ReDim", "ReadOnly", "RemoveHandler", "Resume", "Return", "SByte",
+            "Select", "Set", "Shadows", "Shared", "Short", "Single", "Static", "Step", "Stop", "String", "Structure", "Sub",
+            "SyncLock", "Then", "Throw", "To", "True", "Try", "TryCast", "TypeOf", "UInteger", "ULong", "UShort", "Using",
+            "Variant", "Wend", "When", "While", "Widening", "With", "WithEvents", "WriteOnly", "Xor", @"\", @"\=", "^", "^="
+        ];
+    }
+}
