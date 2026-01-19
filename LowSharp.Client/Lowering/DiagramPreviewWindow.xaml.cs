1087ef395058c9eab2d30fb386b7b8e641151f80
webmaster442 <ruzsinszki.gabor@gmail.com>
2026-01-19 20:19:55 +0100

    Visualization basics

---
+++ LowSharp.Client/Lowering/DiagramPreviewWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using MahApps.Metro.SimpleChildWindow;
+
+namespace LowSharp.Client.Lowering;
+/// <summary>
+/// Interaction logic for DiagramPreviewWindow.xaml
+/// </summary>
+public partial class DiagramPreviewWindow : ChildWindow
+{
+    public DiagramPreviewWindow()
+    {
+        InitializeComponent();
+    }
+}
--- Lowsharp.Server/Program.cs
+++ Lowsharp.Server/Program.cs
@@ -1,6 +1,8 @@
 using Lowsharp.Server.CodeExamples;
 using Lowsharp.Server.Infrastructure;
 using Lowsharp.Server.Lowering;
+using Lowsharp.Server;
+using Microsoft.AspNetCore.StaticFiles;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,7 +22,11 @@ app.MapGrpcService<Lowsharp.Server.Services.ApiV1.RegexService>();
 app.MapGrpcService<Lowsharp.Server.Services.ApiV1.ExamplesService>();
 
 // Configure the HTTP request pipeline.
+var contentTypeProvider = new FileExtensionContentTypeProvider();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+app.MapEmbeddedFile("script/graphere.js", "Lowsharp.Server.Visualization.script.graphere.js", contentTypeProvider);
+app.MapEmbeddedFile("script/nomnoml.js", "Lowsharp.Server.Visualization.script.nomnoml.js", contentTypeProvider);
+
 
 if (!HasReferencePacks())
 {
