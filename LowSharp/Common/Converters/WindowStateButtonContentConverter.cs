f22f1ae84752e5bd26d7099f8c6727f6e8657483
webmaster442 <ruzsinszki.gabor@gmail.com>
2026-02-01 16:57:17 +0100

    Main Window improvements

---
+++ LowSharp/Common/Converters/WindowStateButtonContentConverter.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+using System.Windows;
+
+namespace LowSharp.Common.Converters;
+
+internal class WindowStateButtonContentConverter : OneWayConverterBase<WindowState, string>
+{
+    protected override string ConvertFrom(WindowState value, object parameter, CultureInfo culture)
+    {
+        return value == WindowState.Maximized 
+            ? "\xE923" 
+            : "\xE922";
+    }
+}
--- LowSharp/MainWindow.xaml.cs
+++ LowSharp/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.Windows;
+using System.Windows.Input;
 
 using CommunityToolkit.Mvvm.Messaging;
 
@@ -33,4 +34,25 @@ public partial class MainWindow : Window
         var index = MainTabControl.SelectedIndex;
         WeakReferenceMessenger.Default.Send(new Messages.CloseTabAtIndex(index));
     }
+
+    private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ChangedButton == MouseButton.Left)
+        {
+            DragMove();
+        }
+    }
+
+    private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        => WindowState = WindowState.Minimized;
+
+    private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState == WindowState.Maximized
+            ? WindowState.Normal
+            : WindowState.Maximized;
+    }
+
+    private void CloseButton_Click(object sender, RoutedEventArgs e)
+        => Close();
 }
